e74673fac4d9376d9951abe02c21b3fffffc9c76
anasmzzm <anasmoazzam21@gmail.com>
2024-02-19 23:55:03 -0500

    added munging code

--- munge.py
+++ munge.py
@@ -1,1 +1,16 @@
 # place your code to clean up the data file below.
+
+import pandas as pd
+
+df = pd.read_csv('nba_games.csv')
+columns = df.columns
+for i in columns:
+    print(i)
+
+impt_cols = ['team', 'team_opp', 'fga', 'fg%', '3p', '3p%', 'ft', 
+             'fta', 'ft%', 'trb', 'ast', 'stl', 'blk', 'tov', 
+             'pf', 'pts', 'ortg', 'drtg', 'season', 'won']
+
+df_2023 = df[df['season'] == 2023]
+cleaned_df = df_2023[impt_cols].copy()
+cleaned_df.to_csv('cleaned_csv')
